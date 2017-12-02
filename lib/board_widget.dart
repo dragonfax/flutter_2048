7b367ff2d9aa67a95208fc09e16afb541314559e
Jason Stillwell <dragonfax@gmail.com>
2017-12-02 14:27:58 -0800

    switch to grid positioning

--- lib/board_widget.dart
+++ lib/board_widget.dart
@@ -13,32 +13,46 @@ class BoardWidget extends StatefulWidget {
 
 class BoardWidgetState extends State<BoardWidget> {
 
-  Widget build(BuildContext context) {
-    return new Column(
-      mainAxisSize: MainAxisSize.min,
-      children: widget.board.getRows().map((row) {
-      return new Row(
-        mainAxisSize: MainAxisSize.min,
-        children: row.map((peice){
-        return new Container(
-          margin: const EdgeInsets.all(3.0),
-            padding: const EdgeInsets.all(8.0),
+  Widget buildCell(int peice) {
+    return new Container(
+      margin: const EdgeInsets.all(3.0),
+        padding: const EdgeInsets.all(8.0),
 
-            width: 60.0,
-          height: 60.0,
-          alignment: Alignment.center,
-          decoration: new BoxDecoration(
-            border: new Border.all(width: 2.0, color: Colors.black),
-            borderRadius: const BorderRadius.all(const Radius.circular(10.0))
-          ),
-          child: new Text(
-            peice == null ? " " : peice.toString(),
-            style: const TextStyle(fontSize: 28.0, fontWeight: FontWeight.bold)
-          )
-        );
-      }).toList());
-    }).toList() );
-
+        width: 60.0,
+      height: 60.0,
+      alignment: Alignment.center,
+      decoration: new BoxDecoration(
+        border: new Border.all(width: 2.0, color: Colors.black),
+        borderRadius: const BorderRadius.all(const Radius.circular(10.0))
+      ),
+      child: new Text(
+        peice == null ? " " : peice.toString(),
+        style: const TextStyle(fontSize: 28.0, fontWeight: FontWeight.bold)
+      )
+    );
   }
 
+  Widget build(BuildContext context) {
+
+    var cellWidth = 80.0;
+
+    var children = <Widget>[];
+    for ( int x = 0; x <= 3; x++ ) {
+      for ( int y = 0; y <= 3; y++ ) {
+        var peice = widget.board.get(x,y);
+        children.add(
+          new Positioned(
+            top: y * cellWidth,
+            height: cellWidth,
+            left: x * cellWidth,
+            width: cellWidth,
+            child: buildCell(peice)
+          )
+        );
+      }
+    }
+
+    return new Container(width: 80.0 * 4, height: 80.0 * 4, child: new Stack( children: children ));
+
+  }
 }
